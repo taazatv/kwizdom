60efac84c2ff446d0f0e9d0baca85170fd9a53b4
Vikram Newar <112379999+taazatv@users.noreply.github.com>
2023-07-10 16:07:08 +0530

    Update change_permissions.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-find /opt/bitnami/apache/Kwizdom/ -type d -exec chmod 0755 {} \;
-find /opt/bitnami/apache/Kwizdom/ -type f -exec chmod 0644 {} \;
-chmod 0755 /opt/bitnami/apache/Kwizdom/
-chown -R bitnami:daemon /opt/bitnami/apache/Kwizdom/
+find /opt/bitnami/apache/htdocs/kwizdom/ -type d -exec chmod 0755 {} \;
+find /opt/bitnami/apache/htdocs/kwizdom/ -type f -exec chmod 0644 {} \;
+chmod 0755 /opt/bitnami/apache/htdocs/kwizdom/
+chown -R bitnami:daemon /opt/bitnami/apache/htdocs/kwizdom/
 
 
 #find /opt/bitnami/apache2/htdocs/index.js -type f -exec chmod 0777 {} \;
