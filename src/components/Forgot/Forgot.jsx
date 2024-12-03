d2d3c7b849745da4a93f3132077a34ef68300ebd
Taazatv2 <taazatv2@gmail.com>
2024-12-03 13:54:04 +0530

    forget school name update

--- src/components/Forgot/Forgot.jsx
+++ src/components/Forgot/Forgot.jsx
@@ -7,17 +7,8 @@ import { Combobox } from "@headlessui/react";
 
 
 const schoolNames = [
-  "KMPHS",
-  "KMPHS High School",
-  "KM High School",
-  "KPA Model",
-  "KVS High School",
-  "Kanchrapara School",
-  "Kalyani High School",
-  "Halisahar high school",
-  "Kalyani Central Model",
-  "Kanchrapara New School",
-  "Naihati High School",
+  "ASIAN INTERNATIONAL SCHOOL",
+  "ABHINAV  BHARTI"
 ];
 
 const Forgot = () => {
